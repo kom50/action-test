26e66d0f9e2b526dd67eb77f2625351d017b8530
kom50 <omprakashkartik82280@gmail.com>
2023-09-28 16:00:10 +0530

    Updated deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git checkout -b main
 git add -A
 git commit -m 'deploy' 
 
-git push -f git@github.com:kom50/action-test.git main:gh-pages
+git push -f https://github.com/kom50/action-test.git main:gh-pages
 
 cd -
