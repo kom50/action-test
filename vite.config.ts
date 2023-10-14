d7e3b6fdbcc030bcd685211564015b16c5bd856f
kom50 <omprakashkartik82280@gmail.com>
2023-10-14 18:48:47 +0530

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/test/' : "/",
+  base: process.env.NODE_ENV === 'production' ? '/action-test' : "/",
   plugins: [vue()],
 })
